e833287d286d5599d8370c6803da0d9a8ba96544
viniciuscfn <c.viniciusmagalhaes@gmail.com>
2021-06-15 22:37:58 -0300

    Função de cadastrar criada. Obtenho as tarefas,  adiciono a nova tarefa no final da lista de tarefas e adiciona todas as tarefas incluindo a nova tarefa no localstorage"

--- gerenciador-de-projetos/src/app/tarefas/shared/tarefa.service.ts
+++ gerenciador-de-projetos/src/app/tarefas/shared/tarefa.service.ts
@@ -8,8 +8,19 @@ export class TarefaService {
 
   constructor() { }
 
+  /*ListarTodos() é função que ira listar todas as tarefas existentes , e se não houver tarefas irá retornar uma array vazia.*/
+
   listarTodos(): Tarefa[]{
     const tarefas = localStorage['tarefas'];
     return tarefas ? JSON.parse(tarefas) : [];
+  }
+
+  /* Aqui eu cria a função para cadastrar tarefas, onde tarefa irá receber os 3 atributos em seguida puxo todas as tarefas existentes, tarefa.id recebera a data  atual e adiciono no final da lista de tarefas a nova tarefa com o id alterado.*/
+  cadastrar(tarefa: Tarefa): void{
+    const tarefas = this.listarTodos();
+    tarefa.id = new Date().getTime();
+    tarefas.push(tarefa);
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+
   }
 }
